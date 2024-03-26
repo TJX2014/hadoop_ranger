98c3ece700e67e6617701cff3e3579acdb3fe181
TJX2014 <tangjinxin@deepexi.com>
2024-03-26 19:15:16 +0800

    ranger image build and up

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,18 @@
 package org.example;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Map map = new HashMap<String, String>(){
+            {
+                put("aa", "bb");
+            }
+        };
 
         File f = new File("/usr/dfs");
         f.exists();
