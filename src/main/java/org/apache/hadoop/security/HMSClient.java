ee9141866808500a767db33cc18cf3ce293b2c28
TJX2014 <tangjinxin@deepexi.com>
2024-04-10 20:59:49 +0800

    hms client and hadoop login

--- src/main/java/org/apache/hadoop/security/HMSClient.java
+++ src/main/java/org/apache/hadoop/security/HMSClient.java
@@ -1,2 +1,40 @@
-package org.apache.hadoop.security;public class HMSClient {
+package org.apache.hadoop.security;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hive.metastore.HiveMetaStoreClient;
+import org.apache.hadoop.hive.metastore.api.Catalog;
+import org.apache.thrift.TException;
+
+import java.io.IOException;
+import java.security.PrivilegedAction;
+import java.util.List;
+
+public class HMSClient {
+
+  public static void main(String[] args) throws TException, IOException {
+    System.setProperty("java.security.krb5.realm", "HADOOP.COM");
+    System.setProperty("java.security.krb5.kdc", "k8s-node1");
+    String principal = "hive-metastore/k8s-node1@HADOOP.COM";
+    String keytab = "/usr/kerberos_hms/hive-metastore.keytab";
+    UserGroupInformation.loginUserFromKeytab(principal, keytab);
+    UserGroupInformation ugi = UserGroupInformation.getLoginUser();
+
+    UserGroupInformation.reset();
+    Configuration conf = new Configuration();
+    conf.set("hive.metastore.uris", "thrift://10.201.0.250:30470");
+    conf.set("hadoop.security.authentication", "simple");
+    UserGroupInformation.setConfiguration(conf);
+    HiveMetaStoreClient client = new HiveMetaStoreClient(conf);
+    List<String> catalogs = client.getCatalogs();
+
+    List<String> catalogs1 = ugi.doAs((PrivilegedAction<List<String>>) () -> {
+      try {
+        return client.getCatalogs();
+      } catch (TException e) {
+        throw new RuntimeException(e);
+      }
+    });
+    List<String> databases = client.getAllDatabases("hdfs_df_wd_148");
+    System.out.println(databases);
+  }
 }
--- src/main/java/org/apache/hadoop/security/HadoopLogin.java
+++ src/main/java/org/apache/hadoop/security/HadoopLogin.java
@@ -1,2 +1,25 @@
-package org.apache.hadoop.security;public class HadoopLogin {
+package org.apache.hadoop.security;
+
+import java.io.IOException;
+
+public class HadoopLogin {
+
+  public static void main(String[] args) throws IOException {
+    String authRule = "\n" +
+        "            RULE:[2:$1@$0](hdfs@.*HADOOP.COM)s/.*/hdfs/\n" +
+        "            RULE:[2:$1@$0](yarn@.*HADOOP.COM)s/.*/yarn/\n" +
+        "            DEFAULT\n" +
+        "        ";
+    HadoopKerberosName.setRules(authRule);
+
+    String krbFile = "/tmp/hdfs_k8s-node1_HADOOP_COM_krb5.conf";
+    System.setProperty("java.security.krb5.conf", krbFile);
+
+    String loginPrincipal = "hdfs/k8s-node1@HADOOP.COM";
+    String keytab = "/tmp/hdfs_k8s-node1_HADOOP_COM.keytab";
+    UserGroupInformation.loginUserFromKeytab(loginPrincipal, keytab);
+    UserGroupInformation ugi = UserGroupInformation.getLoginUser();
+
+    System.out.println(ugi);
+  }
 }
